62a05678dd465653ee672ddaca8246f7bc034bfc
Conklaven <jacob.klaven@gmail.com>
2022-02-02 23:18:17 +0200

    responsive

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -7,7 +7,7 @@ const SearchBox = (props =>{
                 <img id="logo" src="https://1kur9t3xffe11yy9in1cqsuu-wpengine.netdna-ssl.com/wp-content/uploads/2020/08/cropped-Kenes-tours_Logo-WHITE-3-2048x803.png" height= "35px"/>
                     <div className="Title">
                         <h2 className="f1">Kenes Tours Database</h2>
-                        <button className="backBTN" onClick={props.backHome}>Return Home</button>
+                        <button className="backBTN" onClick={props.backHome}><span id="return">Return Home</span> <span id="arrow"><i class="arrow left"></i></span></button>
                     </div>
             </div>
             <div className="search">
